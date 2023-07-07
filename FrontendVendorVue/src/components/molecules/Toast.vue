84b80e4de639553bb8637101fd08aa35a0333f13
po3rdlp <leonpolandos53@gmail.com>
2023-07-07 11:40:49 +0800

    update

---
+++ FrontendVendorVue/src/components/molecules/Toast.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="toast toast-top toast-end" v-if="showToast">
+    <div class="alert alert-error">
+      <span>{{ message }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: "",
+    },
+    showToast: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
