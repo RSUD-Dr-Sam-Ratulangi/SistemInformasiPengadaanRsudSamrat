cec4094eb15411a030d0bfe269e2926b402a1280
GilbyKoloay <lolwtf10001@gmail.com>
2023-07-12 01:40:04 +0800

    fix print button in modal payout details

--- FrontendRSReact/src/components/orderPages/ModalPayoutDetails.js
+++ FrontendRSReact/src/components/orderPages/ModalPayoutDetails.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { MdMoney } from 'react-icons/md';
+import html2pdf from 'html2pdf.js';
+import logo from '../../assets/images/logo.jpg';
 
 const ModalPayoutDetails = ({ payoutDetails }) => {
   useEffect(() => {
@@ -24,6 +26,96 @@ const ModalPayoutDetails = ({ payoutDetails }) => {
   const time = payoutDetails.orderDate.split('T')[1].split('.')[0];
 
   console.log('payoutDetails', payoutDetails);
+
+  function handlePrintOnClick(e) {
+    e.preventDefault();
+
+    console.log('handlePrintOnClick', payoutDetails);
+
+    let tableBody = '';
+    payoutDetails.orderItems.forEach(item => {
+      tableBody += `
+        <tr>
+          <td>${item.product.name}</td>
+          <td>${item.quantity}</td>
+          <td>${item.bidPrice}</td>
+          <td>${item.totalAmount}</td>
+        </tr>
+      `;
+    });
+
+    const HTMLToBeConvertedToPDF = `
+      <style>
+        @page {
+          size: letter;
+          margin: 1in;
+        }
+        body {
+          font-family: Arial, sans-serif;
+        }
+        h2 {
+          text-align: center;
+        }
+        table {
+          width: 100%;
+          margin-top: 20px;
+          border-collapse: collapse;
+        }
+        table td,
+        table th {
+          padding: 8px;
+          border: 1px solid #000;
+        }
+        table th {
+          background-color: #f2f2f2;
+          font-weight: bold;
+        }
+        img {
+          max-width: 50px;
+        }
+      </style>
+      <body>
+        <p style="text-align: center; line-height: 1; margin-bottom: 5px;">
+          <img src=${logo} alt="Logo" className="logo" style="float: left; margin-right: 10px; height: 50px;">
+          <strong style="font-size: 16px;">PEMERINTAH KABUPATEN MINAHASA</strong>
+        </p>
+        <p style="text-align: center; line-height: 1; margin-bottom: 5px;">
+          <strong style="font-size: 14px;">RUMAH SAKIT UMUM DAERAH DR. SAM RATULANGI TONDANO</strong>
+        </p>
+        <p style="text-align: center; font-size: 12px; line-height: 1;">
+          Jl. Suprapto Luaan Kecamatan Tondano Timur Telp. (0431) 321171 Fax. (0431) 321172
+        </p>
+        <hr style="border: none; height: 1px; background-color: #444444; opacity: 0.5; margin: 10px 0;">
+
+        <h2 style="text-align: center;"><b>Payout Details (${payoutDetails.id})</b></h2>
+          <h3>Date: ${payoutDetails.orderDate.split('T')[0]}</h3>
+          <h3>Time: ${payoutDetails.orderDate.split('T')[1]}</h3>
+          <table>
+            <tr>
+              <th>Product name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Total</th>
+            </tr>
+            ${tableBody}
+            <tr>
+              <th>Total</th>
+              <th></th>
+              <th></th>
+              <th>Rp ${payoutDetails.payment.amount}</th>
+            </tr>
+          </table>
+      </body>
+    `;
+
+    const element = document.createElement("div");
+    element.innerHTML = HTMLToBeConvertedToPDF;
+    const options = {
+      margin: [20, 20, 20, 20],
+    };
+
+    html2pdf().set(options).from(element).save();
+  }
 
   return (
     <dialog id='payoutModal' className='modal'>
@@ -88,7 +180,10 @@ const ModalPayoutDetails = ({ payoutDetails }) => {
 
         {/* Footer */}
         <div className='modal-action'>
-          <button className='text-white btn border-primary-1 bg-primary-1 hover:bg-primary-2 hover:border-primary-2'>
+          <button
+            className='text-white btn border-primary-1 bg-primary-1 hover:bg-primary-2 hover:border-primary-2'
+            onClick={e => handlePrintOnClick(e)}
+          >
             Print
           </button>
         </div>
